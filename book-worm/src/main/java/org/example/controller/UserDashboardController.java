25c35baa7b38e1ade9b2ebc34f3f6f0cd2368ffd
senumiminodya <senumiminodya456@gmail.com>
2024-03-11 11:19:15 +0530

    implement navigations

--- book-worm/src/main/java/org/example/controller/UserDashboardController.java
+++ book-worm/src/main/java/org/example/controller/UserDashboardController.java
@@ -1,4 +1,25 @@
 package org.example.controller;
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 
 public class UserDashboardController {
+    @FXML
+    private JFXButton returnBooksBtn;
+    @FXML
+    void returnBooksBtnOnAction(ActionEvent event) throws IOException {
+        Parent load = FXMLLoader.load(getClass().getResource("/view/returnBooks.fxml"));
+        Scene scene = new Scene(load);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.setTitle("Return Books");
+        stage.show();
+    }
 }
